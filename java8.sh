1e256428ebe0eea1551a262a734d1cbf896913bf
qianlizeguo <1723877247@qq.com>
2019-04-30 17:33:23 +0800

    fix

---
+++ java8.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+
+echo ">>> Checking JAVA8"
+
+if [ ! -d "/usr/local" ]; then
+
+    echo ">>> Installing JAVA8"
+
+    cd /usr/local
+
+    if [ ! -f "jdk-8u141-linux-x64.tar.gz" ]; then
+        wget --no-cookies \
+            --no-check-certificate \
+            --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; \
+            oraclelicense=accept-securebackup-cookie" \
+            "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
+    fi
+
+    if [ ! -d "jdk1.8.0_141" ]; then
+        tar xzf jdk-8u141-linux-i586.tar.gz
+    fi
+
+    mv jdk1.8.0_141 /usr/local/java8
+
+    echo ">>>> start configure"
+
+	echo "JAVA_HOME=/etc/local/java8" >> /etc/profile
+	echo "JRE_HOME=\$JAVA_HOME/jre" >> /etc/profile
+	echo "PATH=\$PATH:\$JAVA_HOME/bin" >> /etc/profile
+	echo "CLASSPATH=.:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar" >> /etc/profile
+	echo "export JAVA_HOME" >> /etc/profile
+	echo "export JRE_HOME" >> /etc/profile
+	echo "export PATH" >> /etc/profile
+	echo "export CLASSPATH" >> /etc/profile
+
+    source /etc/profile
+
+    echo ">>>java8 installed"
+
+    java -version
+fi
---
+++ maven.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+echo ">>> Checking maven"
+
+if [ ! -d "/usr/local" ]; then
+
+    echo ">>> Installing maven"
+
+    cd /usr/local
+
+    if [ ! -f "apache-maven-3.1.1-bin.tar.gz" ]; then
+        wget http://mirrors.hust.edu.cn/apache/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz
+    fi
+
+    if [ ! -d "apache-maven-3.1.1" ]; then
+        tar xzf jdk-8u141-linux-i586.tar.gz
+    fi
+
+    mv apache-maven-3.1.1 /usr/local/maven3
+
+    echo ">>>> start configure"
+
+    echo "export M2_HOME=/usr/local/maven3" >> /etc/profile
+    echo "export PATH=\$PATH:\$JAVA_HOME/bin:\$M2_HOME/bin" >> /etc/profile
+
+    source /etc/profile
+
+    echo ">>>maven installed"
+
+    mvn -v
+fi
